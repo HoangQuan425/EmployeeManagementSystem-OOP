cd8732dc6ba9f914c47a33e74586250421b28c98
QuanHQ <quanqhhq@gmail.com>
2025-07-10 15:19:49 +0700

    add subclasses developer, manager

---
+++ Entity/Developer.cs
@@ -0,0 +1,45 @@
+﻿using OOPDay4.Interfaces;
+
+namespace OOPDay4.Entity
+{
+	public class Developer : Employee, IWork, IReportToManager
+	{
+		public Developer(string id, string name, int age, DateOnly dateBirth, DateOnly hiredDate, DateOnly? leaveDate, decimal baseSalary, decimal allowance, decimal bonus, decimal tax, decimal overTimePay) : base(id, name, age, dateBirth, hiredDate, leaveDate, baseSalary, allowance, bonus, tax, overTimePay)
+		{
+		}
+
+		public void DoWork()
+		{
+			Console.WriteLine($"Developer: {Name} is working on their assigned task, code and fix bug.");
+		}
+
+		public Report ReportToManager(string managerId, string message)
+		{
+			//return $"developer {Name} report to their manager with id: {managerId}";
+			if(managerId == null)
+			{
+				throw new ArgumentNullException(nameof(managerId), "manager id is null or empty");
+			}
+			Report report = new Report(Id, managerId, message, DateTime.UtcNow);
+			return report;
+		}
+
+		protected override decimal CalculateSalary()
+		{
+			decimal NetSalary = (BaseSalary + Allowance + Bonus + OverTimePay) - Tax;
+			if (NetSalary < 0) throw new ArgumentException("Net salary cannot be negative.");
+			return NetSalary;
+		}
+
+		public override void ShowInfo()
+		{
+			Console.WriteLine($"id: {Id}");
+			Console.WriteLine($"name: {Name}");
+			Console.WriteLine($"age: {Age}");
+			Console.WriteLine($"Date birth: {DateBirth}");
+			Console.WriteLine($"hired date: {HiredDate}");
+			Console.WriteLine($"leave date: {LeaveDate}");
+
+		}
+	}
+}
---
+++ Entity/Manager.cs
@@ -0,0 +1,34 @@
+﻿using OOPDay4.Interfaces;
+
+namespace OOPDay4.Entity
+{
+	public class Manager : Employee, IWork
+	{
+		public Manager(string id, string name, int age, DateOnly dateBirth, DateOnly hiredDate, DateOnly? leaveDate, decimal baseSalary, decimal allowance, decimal bonus, decimal tax, decimal overTimePay) : base(id, name, age, dateBirth, hiredDate, leaveDate, baseSalary, allowance, bonus, tax, overTimePay)
+		{
+		}
+
+		protected override decimal CalculateSalary()
+		{
+			decimal LeaderShip = (BaseSalary + Bonus) * 5m/100m;
+			decimal NetSalary = (BaseSalary + Allowance + Bonus + LeaderShip + OverTimePay) - Tax;
+			if (NetSalary < 0) throw new ArgumentException("Net salary cannot be negative.");
+			return NetSalary;
+		}
+
+		public override void ShowInfo()
+		{
+			Console.WriteLine($"id: {Id}");
+			Console.WriteLine($"name: {Name}");
+			Console.WriteLine($"age: {Age}");
+			Console.WriteLine($"Date birth: {DateBirth}");
+			Console.WriteLine($"hired date: {HiredDate}");
+			Console.WriteLine($"leave date: {LeaveDate}");
+		}
+
+		public void DoWork()
+		{
+			Console.WriteLine($"Manager: {Name} is doing their job to assign task and manage the team");
+		}
+	}
+}
